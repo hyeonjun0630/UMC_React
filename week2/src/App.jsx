b1db1b67e04184f570aac2cd6a5d0ae00b93b3d0
Haru0630 <hyeonjun@inha.edu>
2024-04-14 15:09:17 +0900

    흠

--- week2/src/App.jsx
+++ week2/src/App.jsx
@@ -1,14 +1,14 @@
 // import Counter from "./Counter/Counter";
-// import ModalReal from "./Modal/ModalReal";
+import ModalReal from "./Modal/ModalReal";
 // import Todo from "./Todo/Todo";
-import Movie from "./Movie/Movie";
+// import Movie from "./Movie/Movie";
 function App() {
   return (
     <>
       {/* <Counter></Counter> */}
-      {/* <ModalReal></ModalReal> */}
+      <ModalReal></ModalReal>
       {/* <Todo></Todo> */}
-      <Movie></Movie>
+      {/* <Movie></Movie> */}
     </>
   );
 }
